25cd5261fdf1e7d8135e0359113b4da8a1cc16aa
codingxllama <osamaalchalabi2@gmail.com>
2023-09-07 23:41:53 -0400

    added the the ability to validate form using js

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const password2_Element = document.getElementById("password2");
 const messageContainer = document.querySelector(".message-container");
 const message = document.getElementById("message");
 const regBtn = document.getElementById("regBtn");
-console.log("hi");
 
 let isValid = false;
 let passwordMatch = false;
@@ -12,19 +11,19 @@ let passwordMatch = false;
 function validateForm() {
   // Using the constraint API
   isValid = form.checkValidity();
-  // alert("jo");
+
   if (!isValid) {
     // styling the main message css for invalid attempt
     message.textContent = "Please Fill Out All Fields!";
     message.style.color = "red";
     messageContainer.style.borderColor = "red";
+    return; // we save computation time by adding these return statements
   }
   // Check to see if passwords are match
   if (password1_Element.value === password2_Element.value) {
     passwordMatch = true;
     password1_Element.style.borderColor = "green";
     password2_Element.style.borderColor = "green";
-    alert("match!");
   } else {
     passwordMatch = false;
     message.textContent = "Make sure passwords match.";
@@ -41,12 +40,31 @@ function validateForm() {
   }
 }
 
+// Creating function to store the form data
+function storeFormData() {
+  // creating user object
+
+  const user = {
+    name: form.name.value,
+    phone: form.phone.value,
+    password: form.password.value,
+    email: form.email.value,
+    website: form.website.value,
+  };
+  // We can use this data to hook it up to a server, db, etc.
+  console.log(user);
+}
+
 function processFormData(e) {
   e.preventDefault(); // prevents the page from refreshing
 
-  // console.log("test");
   // Validating the from
   validateForm();
+
+  // We only want to submit our data if it's valid
+  if (isValid && passwordMatch) {
+    storeFormData();
+  }
 }
 
 // Event Listener
